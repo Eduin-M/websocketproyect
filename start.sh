fadd140a6931db150b707cd4fe5b356aafddec29
Eduin-M <168369248+Eduin-M@users.noreply.github.com>
2025-07-07 16:54:18 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-php notificaciones-server.php
+
+# Arranca el WebSocket en segundo plano
+php notificaciones-server.php &
+
+# Sirve archivos PHP como ping.php en el puerto asignado por Railway
+php -S 0.0.0.0:$PORT
